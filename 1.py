fc480a74cc9cd23ecfb4fe316ddff07c648964e4
gianfernandez105 <115988585+gianfernandez105@users.noreply.github.com>
2023-01-09 10:21:00 +0100

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,12 @@
+import math
+
+acabar = False
+
+
+print ("Escriu la sequència de naturals. Escriu 0 per acabar la llista:")
+while (not acabar)
+    nombre = int ( input("nombre? "))
+    if (nombre == 0 ):
+        acabar = true
+    else:
+    print    
